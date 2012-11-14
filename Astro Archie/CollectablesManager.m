67635a410570e2c98d6b5f569e09306670b868f6
benWaxman <b0030196@my.shu.ac.uk>
2012-11-14 13:00:56 +0000

    Triangles and diamonds done and activated Circle code mostly done but acting a bit weird so won't be called at the mo.

--- Astro Archie/CollectablesManager.m
+++ Astro Archie/CollectablesManager.m
@@ -125,13 +125,13 @@
         //[self drawCircle];
         //break;
       case 1:
-        //[self drawTriangle];
-        //break;
-      case 2:
-        //[self drawDiamond];
-        //break;
-      case 3:
         [self drawSquare];
+        break;
+      case 2:
+        [self drawDiamond];
+        break;
+      case 3:
+        [self drawTriangle];
         break;
       default:
         [self drawRandomLine];
@@ -169,22 +169,23 @@
 
 -(void)drawSquare
 {
-  CGPoint startPos, pos;
-  startPos.x = arc4random_uniform(0.7 *_screenSize.width) + (0.1 * _screenSize.width);
-  startPos.y = (arc4random_uniform(50) + 50) + _highestCoin.y;
   float largestSpriteHeight = 0;
   float largestSpriteWidth= 0;
   //get size info for spacing out the square so there is no overlapping sprites
   for(int i = 0; i < 9; i++){
     Coin *coin = [[self hiddenCoins] objectAtIndex:i];
     largestSpriteHeight = MAX(largestSpriteHeight, coin.spriteBox.size.height);
-    largestSpriteWidth = MAX(largestSpriteWidth, coin.spriteBox.size.width);    
+    largestSpriteWidth = MAX(largestSpriteWidth, coin.spriteBox.size.width);
   }
   //space it out a little more
   largestSpriteWidth *= 1.1;
   largestSpriteHeight *= 1.1;
+  
+  CGPoint startPos, pos;
+  startPos.x = arc4random_uniform(0.7 *_screenSize.width) + (0.1 * _screenSize.width);
+  startPos.y = (arc4random_uniform(50) + 50) + _highestCoin.y;
   //keep it all onscreen
-  if((startPos.x + 3 * largestSpriteWidth) > _screenSize.width){
+  while((startPos.x + 3 * largestSpriteWidth) > _screenSize.width + (0.5 * largestSpriteWidth)){
     startPos.x -= largestSpriteWidth;
   }
   //draw the square
@@ -206,17 +207,147 @@
 
 -(void)drawCircle
 {
-  //code to draw circle
+  int numberOfObjects = 8;
+  float largestSpriteHeight = 0;
+  float largestSpriteWidth= 0;
+  //get size info for spacing out the square so there is no overlapping sprites
+  for(int i = 0; i < numberOfObjects; i++){
+    Coin *coin = [[self hiddenCoins] objectAtIndex:i];
+    largestSpriteHeight = MAX(largestSpriteHeight, coin.spriteBox.size.height);
+    largestSpriteWidth = MAX(largestSpriteWidth, coin.spriteBox.size.width);
+  }
+  //space it out a little more
+  largestSpriteWidth *= 1.1;
+  largestSpriteHeight *= 1.1;
+  
+  CGPoint startPos, pos;
+  startPos.x = arc4random_uniform(0.7 *_screenSize.width) + (0.1 * _screenSize.width);
+  startPos.y = (arc4random_uniform(50) + 50) + _highestCoin.y + largestSpriteHeight;
+  //keep the circle onscreen
+  while((startPos.x + 2.5 * largestSpriteWidth) > _screenSize.width){
+    startPos.x -= 0.5 * largestSpriteWidth;
+  }
+  while((startPos.x - 2.5 * largestSpriteWidth) < 0){
+    startPos.x += 0.5 * largestSpriteWidth;
+  }
+  //start angle and stepping for circular distribution
+  float angle = 0.0f;
+  float angleStep = 360.0f/numberOfObjects;
+  
+  for(int i=0; i < numberOfObjects; i++){
+    Coin *selectedCoin = [[self hiddenCoins] objectAtIndex:i];
+    pos.x = startPos.x + (1.8 * largestSpriteWidth * cosf(angle));
+    pos.y = startPos.y + (1.8 * largestSpriteWidth * sinf(angle));
+    selectedCoin.sprite.position = pos;
+    [[self visibleCoins] addObject:selectedCoin];
+    angle += angleStep;
+    NSLog(@"angle = %f", angle);
+  }
+  
+  _highestCoin.x = startPos.x;
+  _highestCoin.y = startPos.y + arc4random_uniform(100) + 2 * largestSpriteHeight;
+  [[self hiddenCoins] removeObjectsInArray:[self visibleCoins]];
+  
 }
 
 -(void)drawDiamond
 {
-  //code to draw diamond
+  float largestSpriteHeight = 0;
+  float largestSpriteWidth= 0;
+  //get size info for spacing out the square so there is no overlapping sprites
+  for(int i = 0; i < 9; i++){
+    Coin *coin = [[self hiddenCoins] objectAtIndex:i];
+    largestSpriteHeight = MAX(largestSpriteHeight, coin.spriteBox.size.height);
+    largestSpriteWidth = MAX(largestSpriteWidth, coin.spriteBox.size.width);
+  }
+  //space it out a little more
+  largestSpriteWidth *= 1.1;
+  largestSpriteHeight *= 1.1;
+  
+  CGPoint pos;
+  float startX;
+  startX = arc4random_uniform(0.7 *_screenSize.width) + (0.1 * _screenSize.width);
+  pos.y  = (arc4random_uniform(50) + 50) + _highestCoin.y;
+  //keep it all onscreen
+  while((startX + 1.5 * largestSpriteWidth) > _screenSize.width){
+    startX -= 0.5 * largestSpriteWidth;
+  }
+  while((startX - 1.5 * largestSpriteWidth) < 0){
+    startX += 0.5 * largestSpriteWidth;
+  }
+  //hidden object array index reference
+  int i = 0;
+  //bottom triangle 1/2
+  for(int y=0; y < 2; ++y){
+    for(int x=0; x <= y; ++x){
+      Coin *selectedCoin = [[self hiddenCoins] objectAtIndex:i];
+      pos.x = startX + (x * largestSpriteWidth);
+      selectedCoin.sprite.position = pos;
+      [[self visibleCoins] addObject:selectedCoin];
+      i++;
+    }
+    startX -= 0.5 * largestSpriteWidth;
+    pos.y += largestSpriteHeight;
+  }  
+  //top triangle 3/2/1
+  for(int y=3; y > 0; --y){
+    for(int x=0; x < y; ++x){
+      Coin *selectedCoin = [[self hiddenCoins] objectAtIndex:i];
+      pos.x = startX + (x * largestSpriteWidth);
+      selectedCoin.sprite.position = pos;
+      [[self visibleCoins] addObject:selectedCoin];
+      i++;
+    }
+    startX += 0.5 * largestSpriteWidth;
+    pos.y += largestSpriteHeight;
+  }
+  
+  _highestCoin.x = pos.x;
+  _highestCoin.y = pos.y + arc4random_uniform(100) + largestSpriteHeight;
+  [[self hiddenCoins] removeObjectsInArray:[self visibleCoins]];
 }
 
 -(void)drawTriangle
 {
-  //code to draw triangle
+  float largestSpriteHeight = 0;
+  float largestSpriteWidth= 0;
+  //get size info for spacing out the square so there is no overlapping sprites
+  for(int i = 0; i < 10; i++){
+    Coin *coin = [[self hiddenCoins] objectAtIndex:i];
+    largestSpriteHeight = MAX(largestSpriteHeight, coin.spriteBox.size.height);
+    largestSpriteWidth = MAX(largestSpriteWidth, coin.spriteBox.size.width);
+  }
+  //space it out a little more
+  largestSpriteWidth *= 1.1;
+  largestSpriteHeight *= 1.1;
+  
+  CGPoint pos;
+  float startX;
+  startX = arc4random_uniform(0.7 *_screenSize.width) + (0.1 * _screenSize.width);
+  pos.y  = (arc4random_uniform(50) + 50) + _highestCoin.y;
+  //keep it all onscreen
+  while((startX + 4 * largestSpriteWidth) > _screenSize.width + (0.5 * largestSpriteWidth)){
+    startX -= 0.5 * largestSpriteWidth;
+  }
+  //hidden object array index reference
+  int i = 0;
+  //draw the triangle 4/3/2/1
+  for(int y=4; y > 0; --y){
+    for(int x=0; x < y; ++x){
+      Coin *selectedCoin = [[self hiddenCoins] objectAtIndex:i];
+      pos.x = startX + (x * largestSpriteWidth);
+      selectedCoin.sprite.position = pos;
+      [[self visibleCoins] addObject:selectedCoin];
+      i++;
+    }
+    startX += 0.5 * largestSpriteWidth;
+    pos.y += largestSpriteHeight;
+  }
+  
+  _highestCoin.x = pos.x;
+  _highestCoin.y = pos.y + arc4random_uniform(100) + largestSpriteHeight;
+  [[self hiddenCoins] removeObjectsInArray:[self visibleCoins]];
+
 }
 
 -(void)dealloc
@@ -224,6 +355,7 @@
   [super dealloc];
   [[self visibleCoins] release];
   [[self hiddenCoins] release];
+  [[self ObjectsToTrash] release];
 }
 
 @end
